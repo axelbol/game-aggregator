3ca48ae4b1281979cfe935d8011e5fa1a82fccf4
axelbol <axel_lp_alvarado@hotmail.com>
2025-02-04 01:07:12 -0400

    footer section

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -45,6 +45,10 @@
             @yield('content')
         </main>
 
-
+        <footer class="border-t border-gray-800">
+            <div class="container mx-auto px-4 py-6">
+                Powered by <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+            </div>
+        </footer>
     </body>
 </html>
