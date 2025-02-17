2c6ccf876b426a3c7a8e502b8dd25cf4a649e370
axelbol <axel_lp_alvarado@hotmail.com>
2025-02-17 00:16:45 -0400

    index page responsive

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -16,18 +16,18 @@
     </head>
     <body class="font-sans bg-gray-900 text-white antialiased">
         <header class="border-b border-gray-800">
-            <nav class="container mx-auto flex items-center justify-between px-4 py-6">
-                <div class="flex items-center">
+            <nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+                <div class="flex flex-col lg:flex-row items-center">
                     <a href="/">
                         <img src="/images/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
                     </a>
-                    <ul class="flex ml-16 space-x-8">
+                    <ul class="flex ml-0 lg:ml-16 space-x-8 mt-6 lg:mt-0">
                         <li><a href="#" class="hover:text-gray-400">Games</a></li>
                         <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
                         <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
                     </ul>
                 </div>
-                <div class="flex items-center">
+                <div class="flex items-center mt-6 lg:mt-0">
                     <div class="relative">
                         <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-3 pl-8 py-1" placeholder="Buscar...">
                         <div class="absolute top-0 flex items-center h-full ml-2">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,14 @@
             Popular Games
         </h2>
 
-        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
             @for ($i = 0; $i < 12; $i++)
                 <div class="game mt-8">
                     <div class="relative inline-block">
                         <a href="#">
                             <img src="/images/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
                         </a>
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full -mb-5 -mr-5">
                             <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
                         </div>
                     </div>
@@ -23,8 +23,8 @@
             @endfor
         </div> <!-- end popular games -->
 
-        <div class="flex my-10">
-            <div class="recently-reviewed w-3/4 mr-32">
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     @for ($i = 0; $i < 3; $i++)
@@ -34,14 +34,13 @@
                                     <img src="/images/luigi.jpg" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
                                 </a>
                                 <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full -mb-5 -mr-5">
-                                {{-- <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px"> --}}
                                     <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
                                 </div>
                             </div>
                             <div class="ml-12">
                                 <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Luigi III Final</a>
                                 <div class="text-gray-400 mt-1">Playstation 4</div>
-                                <div class="mt-6 text-gray-400">
+                                <div class="mt-6 text-gray-400 hidden lg:block">
                                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, tenetur mollitia earum animi ipsa quas aliquam reiciendis qui suscipit repudiandae obcaecati laudantium maiores reprehenderit inventore voluptas in accusamus amet! Non, quas laudantium. Delectus animi beatae quod consequatur tempora modi architecto obcaecati incidunt saepe ducimus, tempore dolorum itaque, earum adipisci esse maiores quidem eligendi reprehenderit maxime voluptates expedita at explicabo veniam?
                                 </div>
                             </div>
@@ -50,7 +49,7 @@
                 </div> <!-- end recently reviewed container -->
             </div> <!-- end recently reviewed -->
 
-            <div class="most-anticipated w-1/4">
+            <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking wide font-semibold">Most Anticipated</h2>
                 <div class="most-anticipated-container space-y-10 mt-8">
                     @for ($i = 0; $i < 4; $i++)
@@ -68,7 +67,7 @@
 
                 <h2 class="text-blue-500 uppercase tracking wide font-semibold mt-12">Coming Soon</h2>
                 <div class="coming-soon-container space-y-10 mt-8">
-                    @for ($i = 0; $i < 3; $i++)
+                    @for ($i = 0; $i < 4; $i++)
                         <div class="game flex">
                             <a href="#">
                                 <img src="/images/doom.jpg" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
